1599cfafdf874e52fbd2766b877b52f8a15404ca
Ruben van der Linde <ruben@conduction.nl>
2019-10-01 08:29:11 +0200

    Updated documentation

--- api/src/Entity/Request.php
+++ api/src/Entity/Request.php
@@ -22,7 +22,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 class Request
 {
     /**
-     * @var \Ramsey\Uuid\UuidInterface
+     * @var \Ramsey\Uuid\UuidInterface $id The UUID identifier of this object
+     * @example e2984465-190a-4562-829e-a8cca81aa35d
      *	 
      * @ApiProperty(
      * 	   identifier=true,
@@ -36,23 +37,67 @@ class Request
      *     }
      * )
      *
+     * @Assert\Uuid
      * @ORM\Id
      * @ORM\Column(type="uuid", unique=true)
      * @ORM\GeneratedValue(strategy="CUSTOM")
      * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
      */
-    private $id;
+	private $id;
+	
+	/**
+	 * @param string $reference The human readable reference for this request
+	 *
+	 * @ApiProperty(
+	 *     attributes={
+	 *         "swagger_context"={
+	 *         	   "description" = "The name of this API",
+	 *             "type"="string",
+	 *             "example"="My component",
+	 *              "maxLength"="255"
+	 *         }
+	 *     }
+	 * )	 *
+	 *
+	 * @Assert\NotNull
+	 * @Assert\Length(
+	 *      max = 255
+	 * )
+	 * @Groups({"read"})
+	 * @ORM\Column(type="string", length=255)
+     * @ApiFilter(SearchFilter::class, properties={"reference": "exact"})
+	 */
+	private $reference;
 
     /**
-     * @Groups({"read", "write"})
-     * @ORM\ManyToOne(targetEntity="App\Entity\RequestType", inversedBy="requests")
-     * @ORM\JoinColumn(nullable=false)
-     * @ApiFilter(SearchFilter::class, properties={"requestType.id": "exact"})
+	 * @var string $requestType The request type agains wich this request should be validated
+     * @example http://vtc.zaakonline.nl/9bd169ef-bc8c-4422-86ce-a0e7679ab67a
+	 *
+	 * @ApiProperty(
+	 *     attributes={
+	 *         "swagger_context"={
+	 *         	   "description" = "The request type agains wich this request should be validated",
+	 *             "type"="string",
+	 *             "format"="uri",
+	 *             "example"="http://vtc.zaakonline.nl/9bd169ef-bc8c-4422-86ce-a0e7679ab67a",
+	 *             "maxLength"="255"
+	 *         }
+	 *     }
+	 * )
+	 * 
+     * @Assert\Url
+     * @Assert\Length(
+     *      max = 255
+     * )
+     * @Groups({"read"})
+     * @ORM\Column(type="string", length=255)
+     * @ApiFilter(SearchFilter::class, properties={"requestType": "exact"})
      */
     private $requestType;
 
     /**
-     * @param string The RSIN of the organisation that ownes this proces
+     * @var string $rsin The RSIN of the organisation that ownes this proces
+     * @example 002851234
      * 
      * @ApiProperty(
      *     attributes={
@@ -72,33 +117,108 @@ class Request
     private $rsin;
 
     /**
+     * @var string $submitter The BSN (if person) or RSIN (if organisation) that submited this request
+     * @example 002851234
+     * 
+     * @ApiProperty(
+     *     attributes={
+     *         "swagger_context"={
+ 	 *         	   "description" = "The BSN (if person) or RSIN (if organisation) that submited this request",
+     *             "type"="string",
+     *             "example"="002851234",
+ 	 *             "maxLength"="255"
+     *         }
+     *     }
+     * )
+     * 
      * @Groups({"read", "write"})
      * @ORM\Column(type="string", length=255)
      */
     private $submitter;
 
     /**
+     * @var boolean $submitterPerson True if the submitters is a person
+     * @example true
+     * 
+     * @ApiProperty(
+     *     attributes={
+     *         "swagger_context"={
+ 	 *         	   "description" = "True if the submitters is a person",
+     *             "type"="boolean",
+     *             "example"=true
+     *         }
+     *     }
+     * )
+     * 
      * @Groups({"read", "write"})
      * @ORM\Column(type="boolean")
      */
-    private $submitterPerson;
+    private $submitterPerson = true;
 
     /**
+     * @var array $properties The actual properties of the request
+     * @example {}
+     * 
+     * @ApiProperty(
+     *     attributes={
+     *         "swagger_context"={
+ 	 *         	   "description" = "The actual properties of the request",
+     *             "type"="array",
+     *             "format"="json",
+     *             "example"={}
+     *         }
+     *     }
+     * )
+     * 
      * @Groups({"read", "write"})
      * @ORM\Column(type="json_array")
      */
     private $properties;
 
     /**
+     * @var array $cases cases from ZGW that are atached to this request
+     * @example []
+     * 
+     * @ApiProperty(
+     *     attributes={
+     *         "swagger_context"={
+ 	 *         	   "description" = "cases from ZGW that are atached to this request",
+     *             "type"="array",
+     *             "example"="[]"
+     *         }
+     *     }
+     * )
      * @Groups({"read", "write"})
      * @ORM\Column(type="array", nullable=true)
      */
     private $cases = [];
-
+    
+    
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string $proces The proces type that made this request
+     * @example http://ptc.zaakonline.nl/9bd169ef-bc8c-4422-86ce-a0e7679ab67a
+     *
+     * @ApiProperty(
+     *     attributes={
+     *         "swagger_context"={
+     *         	   "description" = "The proces type that made this reques",
+     *             "type"="string",
+     *             "format"="uri",
+     *             "example"="http://ptc.zaakonline.nl/9bd169ef-bc8c-4422-86ce-a0e7679ab67a",
+     *             "maxLength"="255"
+     *         }
+     *     }
+     * )
+     *
+     * @Assert\Url
+     * @Assert\Length(
+     *      max = 255
+     * )
+     * @Groups({"read"})
+     * @ORM\Column(type="string", length=255)
+     * @ApiFilter(SearchFilter::class, properties={"proces": "exact"})
      */
-    private $proces;
+    private $process;
 
     /**
      * @ORM\Column(type="datetime", nullable=true)
@@ -107,7 +227,19 @@ class Request
 
     public function getId()
     {
-        return $this->id;
+    	return $this->id;
+    }
+    
+    public function getReference(): ?string
+    {
+    	return $this->reference;
+    }
+    
+    public function setReference(string $reference): self
+    {
+    	$this->reference = $reference;
+    	
+    	return $this;
     }
 
     public function getRequestType(): ?RequestType
@@ -182,14 +314,14 @@ class Request
         return $this;
     }
 
-    public function getProces(): ?string
+    public function getProcess(): ?string
     {
-        return $this->proces;
+        return $this->process;
     }
 
-    public function setProces(?string $proces): self
+    public function setProcess(?string $process): self
     {
-        $this->proces = $proces;
+        $this->process = $process;
 
         return $this;
     }
